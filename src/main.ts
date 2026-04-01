0cf60eaef24f307d9f5ac07781a5a4d76b7e2bdc
mziyan-dev <m.ziyan@paz.agency>
2026-04-01 15:34:55 +0500

    Refactor main.ts to improve CORS configuration and formatting

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,25 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+  app.enableCors({
+    origin: ['http://localhost:3000'], // frontend URL
+    credentials: true,
+  });
+
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true,
-  }),
-);
-    app.setGlobalPrefix('api/v1');
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
-  // app.enableShutdownHooks();
 }
 
 bootstrap();
